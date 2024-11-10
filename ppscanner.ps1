d4a65d84980f03ada8ae10508da445ccd787ffce
kvass <60018788+kva55@users.noreply.github.com>
2024-11-09 23:33:01 -0700

    Update ppscanner.ps1

--- ppscanner.ps1
+++ ppscanner.ps1
@@ -12,8 +12,8 @@ $common = @(
 	7,20,21,22,23,
 	25,53,69,80,88,
 	102,110,135,137,
-	138,139,143,381,
-	383,443,445,464,2869
+	138,139,143,381,465,
+	383,443,445,464,2869,
 	587,593,636,691,5357,
 	902,989,995,1025,3389,
 	1194,1337,1433,2179,
@@ -25,11 +25,13 @@ $common = @(
 	6667,6668,6669,6881,
 	6999,6970,8080,8081,
 	8082,8087,8222,9100,
-	10000,12345,27374,31337
+	10000,12345,27374,31337,
+    	2052,2053,2082,2083,2086,8443,8880
 )
 
+# Common cloudflare ports
 $sample= @(
-	135
+	2052,2082,2086,8880,8080,8443
 )
 
 $allports = 1..65535
@@ -39,11 +41,18 @@ $allports = 1..65535
 # Store ips and ports
 $ipPortDict = @{}
 
-$suppressClosedPorts = "true"
-#$global_timeoutSec = 15 # set timeout
-$global_timeoutSec_ = 30
-$global_timeoutSec = New-TimeSpan -Seconds $global_timeoutSec_
-$global_fastTimeout = 5 # If unable to connect faster than other requests - filtered
+$suppressClosedPorts = "false"
+
+# global timeout, if longer than 45 seconds to respond, likely filtered
+$global_timeoutSec = New-TimeSpan -Seconds 45
+
+# global slow response - if responds after 15 seconds likely filtered
+$global_slowtimeout = New-TimeSpan -Seconds 15
+$slow_infer = "true"
+
+# global fast response, if responds within 1 second likely filtered or up
+$global_fastTimeout = New-TimeSpan -Second 1 # If unable to connect faster than other requests - filtered
+$fast_infer = "true"
 
 Function smtp_portscan
 {
@@ -86,10 +95,10 @@ Function smtp_portscan
         Write-Output "[MAIL] $t - Open|filtered"
         $ipPortDict[$target] += @($port)
     }
-    if($resp -like "Unable to read data from the transport connection: An existing connection was forcibly closed by the remote host.")
+    if($resp -like "Unable to read data from the transport connection: An existing connection was forcibly closed by the remote host." -and $suppressClosedPorts -eq "false")
     {
-        Write-Output "[MAIL] $t - open"
-        $ipPortDict[$target] += @($port)
+        Write-Output "[MAIL] $t - closed"
+        #$ipPortDict[$target] += @($port)
     }
     if($resp -like "Unable to read data from the transport connection: net_io_connectionclosed.")
     {
@@ -124,13 +133,38 @@ Function http_portscan
     # enable for debugging
     #Write-Output $resp
     
+    $end1 = Get-Date
+    $elapsed1 = $end1 - $start
+    #$elapsed1_format = $elapsed1 -Format "HH:mm:ss.fff"
+
+    if($fast_infer -eq "true")
+    {
+        # Maybe higher FP - responses shorter than avg
+        if($resp -like "Unable to connect to the remote server" -and $elapsed1 -lt $global_fastTimeout)
+        {
+            Write-Output "[HTTP] $t - open|filtered"
+            #Write-Output "aaa"
+            $ipPortDict[$target] += @($port)
+        }
+    }
+
+    if($slow_infer -eq "true")
+    {
+        # Maybe higher FP - requests longer than avg
+        if($resp -like "Unable to connect to the remote server" -and $elapsed1 -gt $global_slowtimeout) #
+        {
+             Write-Output "[HTTP] $t - open|filtered"
+            $ipPortDict[$target] += @($port)
+        }
+    }
+
     if($resp -like "Unable to connect to the remote server" -and $suppressClosedPorts -eq "false")
     {
         Write-Output "[HTTP] $t - closed"
     }
-    $end1 = Get-Date
-    $elapsed1 = $end1 - $start
+    
     #Write-Output $elapsed1 #uncomment for debugging
+    #Write-Output $global_fastTimeout #uncomment for debugging
     if($resp -like "The operation has timed out." -and $elapsed1 -ge $global_timeoutSec)
     {
         Write-Output "[HTTP] $t - open|filtered"
@@ -141,10 +175,16 @@ Function http_portscan
         Write-Output "[HTTP] $t - open"
         $ipPortDict[$target] += @($port)
     }
-    if($resp -like "The underlying connection was closed: An unexpected error occurred on a receive.")
+    #May lead to higher FP
+    #if($resp -like "The underlying connection was closed: An unexpected error occurred on a receive." -and $elapsted1 -lt $global_fastTimeout)
+    #{
+    #    Write-Output "[HTTP] $t - open|filtered"
+    #    $ipPortDict[$target] += @($port)
+    #}
+    if($resp -like "The underlying connection was closed: An unexpected error occurred on a receive."  -and $suppressClosedPorts -eq "false")
     {
-        Write-Output "[HTTP] $t - open"
-        $ipPortDict[$target] += @($port)
+        Write-Output "[HTTP] $t - closed"
+        #$ipPortDict[$target] += @($port)
     }
     if($resp -like "The underlying connection was closed: An unexpected error occurred on a send.")
     {
@@ -162,6 +202,21 @@ Function http_portscan
         $ipPortDict[$target] += @($port)
     }
     if($resp -like "The server committed a protocol violation. Section=ResponseStatusLine")
+    {
+        Write-Output "[HTTP] $t - open"
+        $ipPortDict[$target] += @($port)
+    }
+    if($resp -like "The remote server returned an error: (403) Forbidden.")
+    {
+        Write-Output "[HTTP] $t - open"
+        $ipPortDict[$target] += @($port)
+    }
+    if($resp -like "The plain HTTP request was sent to HTTPS port")
+    {
+        Write-Output "[HTTP] $t - open"
+        $ipPortDict[$target] += @($port)
+    }
+    if($resp -like "The remote server returned an error: (400) Bad Request.")
     {
         Write-Output "[HTTP] $t - open"
         $ipPortDict[$target] += @($port)
@@ -256,13 +311,13 @@ foreach ($target in $targetList)
     Write-Output ""
     Write-Output "Starting Portscan on $target"
     
-    foreach ($port in $common)
+    foreach ($port in $sample)
     {
 
         $c = Get-Random -Minimum 1 -Maximum 4 # random function each time
 	    #$c = 3 # for full wsman scan
-        $c = 2 # for full http scan
-        #$c = 1 # for full smtp scan
+        #$c = 2 # for full http scan
+        $c = 1 # for full smtp scan
         
 	    #Write-Output  $t
 	    if($c -eq 1)
