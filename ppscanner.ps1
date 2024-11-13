24949ba14e01b9c5600c4b9dd0e10c3675173214
kvass <60018788+kva55@users.noreply.github.com>
2024-11-12 19:38:32 -0700

    Update ppscanner.ps1

--- ppscanner.ps1
+++ ppscanner.ps1
@@ -6,65 +6,159 @@ Write-Output "github: https://github.com/kva55/ppscanner"
 Write-Output ""
 
 # Enter ip in arg ppscanner.ps1 192.168.1.1
-if ($args[0] -ne $null -and $args[0] -ne "") 
-{ 
-    $targetList = $args[0]
-    $targetList = $targetList -replace " ", "" # remove all spaces
-    $targetList = $targetList -split ","       #split list with comma delimiter
-    Write-Output "Proceeding to scan:"
-    Write-Output $targetList
-} 
-else 
-{ 
-    Write-Output "No targets specified with arg. Proceeding with:"
-    $targetList = @(
-    #"192.168.1.1"
-    #"192.168.2.2",
-    #"192.168.3.3"
-    ) 
 
-    Write-Output $targetList
-    Write-Output ""
+# get ports
+for ($i = 0; $i -lt $args.Length; $i++)
+{
+    if ($args[$i] -eq "--port" -or $args[$i] -eq "-p")
+    {
+        $portIndex = $args.IndexOf("--port")
+        if ($portIndex -eq -1) 
+        {
+            $portIndex = $args.IndexOf("-p")
+            if ($portIndex -ne -1 -and $portIndex + 1 -lt $args.Length)
+            {
+                $portList = $args[$portIndex + 1] 
+                $portList = $portList -replace " ", "" # remove all spaces 
+                $portList = $portList -split "," #split list with comma delimiter
+                Write-Output "Selected ports: "
+                Write-Output $portList
+                Write-Output "" 
+                
+            } 
+        }
+        else 
+        {
+            Write-Output "No port values. Proceeding with common ports."
+            # Set of common ports
+            $portList = @(
+	        7,20,21,22,23,
+	        25,53,69,80,88,
+	        102,110,135,137, 
+            	138,139,143,381,465,
+	    	383,443,445,464,2869,  
+            	587,593,636,691,5357,
+            	5040,902,912,989,995,
+            	1025,3389,1194,1337,
+            	1433,2179,4022,1434,
+            	1589,1725,1900,5353,
+            	5780,3702,2083,2483,
+            	2484,2967,3074,3306,
+            	3724,46645432,5900,
+            	6665,6666,6667,6668,
+            	6669,6881,6999,6970,
+            	8080,8081,8082,8087,
+            	8222,9100,10000,12345,
+            	27374,31337,2052,2053,
+            	2082,2083,2086,8443,8880
+            )
+            #Write-Output $portList
+        }
+    }
 }
 
-# Set of common ports
-$common = @(
-	7,20,21,22,23,
-	25,53,69,80,88,
-	102,110,135,137,
-	138,139,143,381,465,
-	383,443,445,464,2869,
-	587,593,636,691,5357,
-    5040,902,912,989,995,
-    1025,3389,1194,1337,
-    1433,2179,4022,1434,
-    1589,1725,1900,5353,
-    5780,3702,2083,2483,
-    2484,2967,3074,3306,
-    3724,46645432,5900,
-    6665,6666,6667,6668,
-    6669,6881,6999,6970,
-    8080,8081,8082,8087,
-    8222,9100,10000,12345,
-    27374,31337,2052,2053,
-    2082,2083,2086,8443,8880
-)
+#get ips
+for ($i = 0; $i -lt $args.Length; $i++)
+{
+    if ($args[$i] -eq "--target" -or $args[$i] -eq "-t")
+    {
+        $ipIndex = $args.IndexOf("--target")
+        if ($ipIndex -eq -1) 
+        {
+            $ipIndex = $args.IndexOf("-t")
+            if ($ipIndex -ne -1 -and $ipIndex + 1 -lt $args.Length)
+            {
+                $targetList = $args[$ipIndex + 1] 
+                $targetList = $targetList -replace " ", "" # remove all spaces 
+                $targetList = $targetList -split "," #split list with comma delimiter
+                Write-Output "Proceeding to scan:"
+                Write-Output $targetList
+                #Write-Output "On ports: "
+                #Write-Output $portList 
+                
+            } 
+            else 
+            {
+                Write-Output "No targets specified with arg. Proceeding with:"
+                $targetList = @(
+                #"192.168.1.1",
+                #"192.168.2.2",
+                #"192.168.3.3",
+                )
+                Write-Output $targetList
+                #Write-Output "On ports: "
+                #Write-Output $portList  
+            }
+        }
+    }
+}
 
-# Common cloudflare ports
-$cfports= @(
-	2052,2082,2086,8880,8080,8443
-)
+# suppress closed ports and show only open ports
+$suppressClosedPorts = "true"
+
+# bools for scan types
+$smpt  = "false"
+$http  = "false"
+$wsman = "false"
+
+Write-Output ""
+#get additional args
+for ($i = 0; $i -lt $args.Length; $i++)
+{
+    # Show cloud ports
+    if ($args[$i] -eq "--closed" -or $args[$i] -eq "-c")
+    {
+        $suppressClosedPorts = "false"
+    }
+
+    # Scan common cloudflare ports
+    if ($args[$i] -eq "--cloudflare-check" -or $args[$i] -eq "-cfc")
+    {
+        # Common cloudflare ports
+        $portList= @(
+	    2052,2082,2086,8880,8080,8443
+        )
+        Write-Output "[*] Proceeding with cloudflare ports"
+    }
+
+    # Scan all ports
+    if ($args[$i] -eq "--all-ports" -or $args[$i] -eq "-p-")
+    {
+        # All ports
+        $portList = 1..65535
+        Write-Output "[*] Proceeding with scanning all ports"
+    }
+
+    if ($args[$i] -eq "--smtp-scan" -or $args[$i] -eq "-smtp")
+    {
+        # All ports
+        $smtp = "true"
+        Write-Output "[*] Only scanning with smtp"
+    }
+
+    if ($args[$i] -eq "--http-scan" -or $args[$i] -eq "-http")
+    {
+        # All ports
+        $http = "true"
+        Write-Output "[*] Only scanning with http"
+    }
+
+    if ($args[$i] -eq "--wsman-scan" -or $args[$i] -eq "-wsman")
+    {
+        # All ports
+        $wsman = "true"
+        Write-Output "[*] Only scanning with wsman"
+    }
+
+}
 
 $sample= @(
 	139, 445, 5357, 902, 912, 443,22
 )
 
-$allports = 1..65535
 
 # Store ips and ports
 $ipPortDict = @{}
-
-$suppressClosedPorts = "true"
 
 # global timeout, if longer than 45 seconds to respond, likely filtered
 $global_timeoutSec = New-TimeSpan -Seconds 45
@@ -315,15 +409,24 @@ foreach ($target in $targetList)
     Write-Output ""
     Write-Output "Starting Portscan on $target"
     
-    foreach ($port in $sample)
+    foreach ($port in $portList)
     {
 
         $c = Get-Random -Minimum 1 -Maximum 4 # random function each time
-	    #$c = 3 # for full wsman scan
-        #$c = 2 # for full http scan
-        #$c = 1 # for full smtp scan
+	    
+        if($smtp -eq "true")
+        {
+            $c = 1 # for full wsman scan
+        }
+        if($http -eq "true")
+        {
+            $c = 2 # for full wsman scan
+        }
+        if($wsman -eq "true")
+        {
+            $c = 3 # for full wsman scan
+        }
         
-	    #Write-Output  $t
 	    if($c -eq 1)
         {
             smtp_portscan -target $target -port $port
